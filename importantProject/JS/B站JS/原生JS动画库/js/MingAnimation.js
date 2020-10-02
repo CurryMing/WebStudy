42daeb744970e8c60e23e8fc3268563bbea47321
o_Ming <1158996774@qq.com>
2020-10-02 17:47:28 +0800

    2020/10/02-18:00

--- importantProject/JS/B站JS/原生JS动画库/js/MingAnimation.js
+++ importantProject/JS/B站JS/原生JS动画库/js/MingAnimation.js
@@ -1,37 +1,44 @@
 // 封装函数
-function tween(obj,name,to,time,fun){
-	var speed = 0
-	var cur = 0
-	clearInterval(obj.timer)
-	obj.timer = setInterval(function() {
-		if(name === 'opacity'){
-			cur = Math.round(parseFloat(getStyle(obj,name)) * 100) 
-		}else {
-			cur = parseInt(getStyle(obj,name))
-		}
-		if(to >= cur){
-			speed = Math.ceil((to-cur) / 30)
-		}else{
-			speed = Math.floor((to-cur) / 30)
-		}
-		// 结束动画
-		if(cur === to){
-			clearInterval(obj.timer)
-			if(fun){
-				fun()
-			}
-			return
-		}
-		if(name === 'opacity'){
-			obj.style[name] = (cur + speed) / 100
-		}else {
-			obj.style[name] = cur + speed + 'px'
-		}
-	},time)
+function tween(obj, json, time, fun) {
+    var speed = 0,
+        curStyle = 0,
+        to = 0
+    clearInterval(obj.timer)
+    obj.timer = setInterval(function() {
+        var flag = true
+        for (var name in json) {
+            to = json[name]
+            if (name === 'opacity') {
+                curStyle = Math.round(parseFloat(getStyle(obj, name)) * 100)
+            } else {
+                curStyle = parseInt(getStyle(obj, name))
+            }
+            if (to >= curStyle) {
+                speed = Math.ceil((to - curStyle) / 15)
+            } else {
+                speed = Math.floor((to - curStyle) / 15)
+            }
+            if (curStyle !== to) {
+                flag = false
+            }
+            if (name === 'opacity') {
+                obj.style[name] = (curStyle + speed) / 100
+            } else {
+                obj.style[name] = curStyle + speed + 'px'
+            }
+        }
+        if (flag) {
+            clearInterval(obj.timer)
+            if (fun) {
+                fun()
+            }
+            return
+        }
+    }, time)
 }
 
 // 封装获取【样式】的函数
-function getStyle(obj,name) {
-	      // IE                                      // 主流  
-	return obj.currentStyle ? obj.currentStyle[name] : window.getComputedStyle(obj,false)[name];
+function getStyle(obj, name) {
+    // IE                                      // 主流  
+    return obj.currentStyle ? obj.currentStyle[name] : getComputedStyle(obj, false)[name];
 }
